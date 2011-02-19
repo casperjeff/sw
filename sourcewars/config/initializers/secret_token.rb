c37e2842cfc6019f59d53d3b0fe70eb8d5c13928
casperjeff <jeff.amiel@gmail.com>
2011-02-19 10:34:31 -0600

    first commit

---
+++ sourcewars/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sourcewars::Application.config.secret_token = '277faeca5e02bd2086a5b04c858dea5bc3af1bcfeaf1444a0f45b5cc9b515e59901b12a8a13b65314f155f9e50eec385b5bc02d35a734c20867664ff9baa43bd'
